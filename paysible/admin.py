55231c01716b1ac517c960e96dd1c03e07182915
TheoGar <theo.garnaud@telecom-sudparis.eu>
2025-11-27 15:04:58 +0100

    database and user

--- paysible/admin.py
+++ paysible/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import BankUser, Transaction, Account, TransactionEntry
 
-# Register your models here.
+admin.site.register(BankUser)
+admin.site.register(Transaction)
+admin.site.register(Account)
+admin.site.register(TransactionEntry)
