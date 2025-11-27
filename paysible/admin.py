0be9115da7e5af2c7281e0098397071b6e9d6387
b3rnvrd <eliottcolson.snir@gmail.com>
2025-11-27 16:57:19 +0100

    passage a FastAPI

--- paysible/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import BankUser, Transaction, Account, TransactionEntry
-
-admin.site.register(BankUser)
-admin.site.register(Transaction)
-admin.site.register(Account)
-admin.site.register(TransactionEntry)
