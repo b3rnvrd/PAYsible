2fbfdfc35a6b1ac65579bc6fea0c473e9a2989ac
ksiksy <alexis.scaglia@gmail.com>
2025-12-03 15:34:51 +0100

    ajout de l'inclusion du routeur des utilisateurs dans le routeur principal

--- app/api/routers.py
+++ app/api/routers.py
@@ -1,7 +1,9 @@
 # app/api/routers.py
 
 from fastapi import APIRouter
+from app.api.endpoints import users
 
 router = APIRouter()
 
 # Les routeurs spécifiques (users.py, accounts.py) seront inclus ici plus tard.
+router.include_router(users.router)
